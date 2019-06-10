b7ee5a9a59e32d369f3d2e90eba6c55af06fa772
Álvaro Mateos <alvaro_mat@yahoo.es>
2019-06-10 16:17:43 +0200

    Create list of mock maps

---
+++ src/app/services/maps/maps.mock.ts
@@ -0,0 +1,7 @@
+import { BaseMap } from 'src/app/model/map';
+
+export const MOCK_MAPS: BaseMap[] = [
+  { id: 1, name: 'Puerta del Sol', URI: 'sol' },
+  { id: 2, name: 'Nuevos Ministerios', URI: 'nuevos_ministerios' },
+  { id: 3, name: 'Ciudad Universitaria', URI: 'ciudad_universitaria' }
+];
