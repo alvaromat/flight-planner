7662b40cf1ace90b1827397c69e56ec8920ab929
Álvaro Mateos <alvaro_mat@yahoo.es>
2019-06-11 21:16:33 +0200

    Add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -16,6 +17,7 @@ import { NewPlanComponent } from './new-plan/new-plan.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent }
     ])
