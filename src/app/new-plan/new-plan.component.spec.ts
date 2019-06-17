e373f0becce1e180640949ae34eb175c0babb979
Álvaro Mateos <alvaro_mat@yahoo.es>
2019-06-17 21:52:02 +0200

    Redesign new-plan and route it correctly

--- src/app/new-plan/new-plan.component.spec.ts
+++ src/app/new-plan/new-plan.component.spec.ts
@@ -4,21 +4,34 @@ import { NewPlanComponent } from './new-plan.component';
 import { MapsService } from '../services/maps/maps.service';
 import { MOCK_MAPS } from '../services/maps/maps.mock';
 import { SharedModule } from '../shared/shared.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PlansService } from '../services/plans/plans.service';
+import { Router } from '@angular/router';
 
 describe('NewPlanComponent', () => {
   let component: NewPlanComponent;
   let fixture: ComponentFixture<NewPlanComponent>;
-  let mapsSpy: jasmine.SpyObj<MapsService>;
+  let mapsService: jasmine.SpyObj<MapsService>;
+  let plansService: jasmine.SpyObj<PlansService>;
+
+  const fakeMap = MOCK_MAPS[0];
 
   beforeEach(async(() => {
-    const spy = jasmine.createSpyObj('MapsService', ['getAll']);
+    const mapsSpy = jasmine.createSpyObj('MapsService', ['getAll']);
+    const plansSpy = jasmine.createSpyObj('PlansService', ['create']);
+    const router = jasmine.createSpyObj('Router', ['navigate']);
 
     TestBed.configureTestingModule({
       imports: [
-        SharedModule
+        SharedModule,
+        RouterTestingModule
       ],
       declarations: [NewPlanComponent],
-      providers: [{ provide: MapsService, useValue: spy }]
+      providers: [
+        { provide: MapsService, useValue: mapsSpy },
+        { provide: PlansService, useValue: plansSpy },
+        { provide: Router, useValue: router }
+      ]
     })
       .compileComponents();
   }));
@@ -26,18 +39,39 @@ describe('NewPlanComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NewPlanComponent);
     component = fixture.componentInstance;
-    mapsSpy = TestBed.get(MapsService);
+    mapsService = TestBed.get(MapsService);
+    plansService = TestBed.get(PlansService);
   });
 
   it('should get the maps on init', () => {
-    mapsSpy.getAll.and.returnValue(MOCK_MAPS);
+    mapsService.getAll.and.returnValue(MOCK_MAPS);
     component.ngOnInit();
     expect(component.maps).toBe(MOCK_MAPS);
   });
 
   it('should select the first map returned by the service', () => {
-    mapsSpy.getAll.and.returnValue(MOCK_MAPS);
+    mapsService.getAll.and.returnValue(MOCK_MAPS);
     component.ngOnInit();
     expect(component.form.controls.map.value).toBe(MOCK_MAPS[0]);
   });
+
+  it('#addPlan should make a call to the plans service and go to home', () => {
+    component.form.setValue({ map: fakeMap, name: 'name' });
+    component.addPlan();
+    expect(plansService.create).toHaveBeenCalled();
+    const newPlan = plansService.create.calls.first().args[0];
+    expect(newPlan.name).toBe('name');
+    expect(newPlan.mapId).toBe(fakeMap.id);
+
+    const router = TestBed.get(Router);
+    expect(router.navigate).toHaveBeenCalledWith(['']);
+  });
+
+  it('#cancel should navigate to home', () => {
+    const router = TestBed.get(Router);
+    component.cancel();
+    expect(router.navigate).toHaveBeenCalledWith(['']);
+  });
+
+
 });
--- src/app/new-plan/new-plan.component.ts
+++ src/app/new-plan/new-plan.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MapsService } from '../services/maps/maps.service';
 import { BaseMap } from '../model/map';
+import { PlansService } from '../services/plans/plans.service';
+import { Plan } from '../model/plan';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-plan',
@@ -13,15 +16,26 @@ export class NewPlanComponent implements OnInit {
   maps: BaseMap[];
 
   form = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('', Validators.required),
     map: new FormControl()
   });
 
-  constructor(private mapsService: MapsService) { }
+  constructor(private mapsService: MapsService, private plansService: PlansService, private router: Router) { }
 
   ngOnInit() {
     this.maps = this.mapsService.getAll();
     this.form.controls.map.setValue(this.maps[0]);
+  }
+
+  addPlan() {
+    const formValue = this.form.value;
+    const plan: Plan = { name: formValue.name, mapId: formValue.map.id, points: [], creation: new Date() };
+    this.plansService.create(plan);
+    this.router.navigate(['']);
+  }
+
+  cancel() {
+    this.router.navigate(['']);
   }
 
 }
