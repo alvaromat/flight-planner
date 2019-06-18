25b704a83017191ccd577edde217cc992b6dcdbf
Álvaro Mateos <alvaro_mat@yahoo.es>
2019-06-18 23:02:38 +0200

    Changed maps

--- src/app/services/maps/maps.mock.ts
+++ src/app/services/maps/maps.mock.ts
@@ -1,7 +1,7 @@
 import { BaseMap } from 'src/app/model/map';
 
 export const MOCK_MAPS: BaseMap[] = [
-  { id: 1, name: 'Puerta del Sol', URI: 'sol' },
+  { id: 1, name: 'Ciudad Universitaria', URI: 'ciudad_universitaria' },
   { id: 2, name: 'Nuevos Ministerios', URI: 'nuevos_ministerios' },
-  { id: 3, name: 'Ciudad Universitaria', URI: 'ciudad_universitaria' }
+  { id: 3, name: 'Puerta del Sol', URI: 'sol' },
 ];
